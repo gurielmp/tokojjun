cb8bc4cbe40fcf33c01dc78525d9b517065802ae
gurielamp <gurielamp@gmail.com>
2024-01-24 06:38:31 +0700

    update logic create new store and existing store

---
+++ app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -0,0 +1,20 @@
+import prismadb from "@/lib/prismadb"
+
+interface DashboardPageProps {
+  params: { storeid: string }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeid,
+    },
+  })
+  return (
+    <>
+      <div>Active store : {store?.name}</div>
+    </>
+  )
+}
+
+export default DashboardPage
