da4d4ce5cc747b23a3f2cce3abd5170aa1acef10
gurielamp <gurielamp@gmail.com>
2024-01-29 16:57:04 +0700

    create data table

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,13 +1,20 @@
 "use client"
 
 import { Plus } from "lucide-react"
+import { useParams, useRouter } from "next/navigation"
 
 import { Button } from "@/components/ui/button"
 import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
-import { useParams, useRouter } from "next/navigation"
+import { DataTable } from "@/components/ui/data-table"
 
-export const BillboardClient = () => {
+import { BillboardColumns, columns } from "./columns"
+
+interface BillboardClientProps {
+  data: BillboardColumns[]
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter()
   const params = useParams()
 
@@ -15,7 +22,7 @@ export const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title="Billboards (0)"
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -26,6 +33,7 @@ export const BillboardClient = () => {
         </Button>
       </div>
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   )
 }
